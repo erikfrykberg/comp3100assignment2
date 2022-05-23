27f61ef5d21a2172ac4df979dfb0066308cf71c3
Erik <erik.frykberg@gmail.com>
2022-05-23 17:43:39 +1000

    extra recieve

--- COMP3100_ASS2.java
+++ COMP3100_ASS2.java
@@ -120,6 +120,8 @@ public class COMP3100_ASS2 {
                 c++;
             }
 
+            recieve();
+
             push("OK"); //after recieving all of the servers!
 
             // by the end of the for loop, we have the smallestIdentification required. There are now 3 steps.
